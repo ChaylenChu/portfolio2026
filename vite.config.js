7726e516dd4e3addff1b908f348163c6a63b5f53
ChaylenChu <chaylenchu@163.com>
2026-03-13 17:48:24 +0800

    eat: add cover images
    feat: add cover images

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: './', // 👈 确保有这一行，这能解决白屏找不到文件的问题
 })
